70cdb84ce9d3f3bf5599ff446d730c60b513219a
xiejinsorry <412546969@qq.com>
2016-01-05 12:52:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/tryagain/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.tryagain;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("kk", "想爱来不及");
+        setContentView(R.layout.activity_main);
+    }
+}
